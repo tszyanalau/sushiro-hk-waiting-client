d40ef4ca93055ba3b7025adaf72be5ee9eb3bd03
Amy Lau <tszyanalau@gmail.com>
2023-08-16 20:37:13 +0900

    run cypress on testing env

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://sushirowaiting-map.tszyanalau.com',
+    baseUrl: 'http://sushiro-hk-waiting-client-testing.s3-website-ap-northeast-1.amazonaws.com',
     viewportWidth: 1280,
     viewportHeight: 800,
   },
